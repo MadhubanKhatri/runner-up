333b4f66511999a07fca4786338c7851ea5b6a02
Madhuban Khatri <madhuban211998@gmail.com>
2021-01-05 00:38:02 +0800

    Update runnerup_score.py

--- runnerup_score.py
+++ runnerup_score.py
@@ -1,61 +1,22 @@
+# Create a Score List
 score_lis = [1,2,3,4,5,6,6,6,7,8,9,9,9,10,10,10,10]
+
+# Print the List
 print('Current Scores: ', score_lis)
 
+# Get Max Number from the list
 max_num = max(score_lis)
+
+# Count the Max Number 
 max_num_count = score_lis.count(max_num)
 
+# Apply for loop and remove Max Number from the list
 for i in range(0, max_num_count):
 	score_lis.remove(max_num)
 
+
+# Now Max Number becomes the Runner Up Score
 runner_up = max(score_lis)
-print('Runner Up Score is: ', runner_up)	
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# score_lis = [1,2,3,8,8,8,7,7,9,9]
-# print('Current Scores: ', score_lis)
-# max_num = max(score_lis)
-# max_num_count = score_lis.count(max_num)
-
-# for i in range(0, max_num_count):
-# 	score_lis.remove(max_num)
-# print('Runner Up Score is: ', max(score_lis))
+# Print it
+print('Runner Up Score is: ', runner_up)
